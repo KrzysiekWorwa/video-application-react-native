e39376d18b6a340ff5671cc22c999c86d62ecd6e
Krzysztof Worwa <worwak1998@gmail.com>
2025-12-01 19:42:46 +0100

    feat: create base theme structure (colors, fonts, fontSizes)

---
+++ theme/theme.ts
@@ -0,0 +1,29 @@
+export const theme = {
+    colors: {
+        background: "#FFFFFF",
+        surface: "#8D99AE",
+
+        textPrimary: "#2B2D42",
+        textSecondary: "#FFFFFF",
+
+        primary: "#2B2D42",
+        secondary: "#FFFFFF",
+    },
+
+    fonts: {
+        regular: "Poppins_400Regular",
+        medium: "Poppins_500Medium",
+        semiBold: "Poppins_600SemiBold",
+        bold: "Poppins_700Bold",
+    },
+
+    fontSizes: {
+        xs: 10,
+        sm: 12,
+        smPlus: 13,
+        md: 14,
+        lg: 16,
+        xl: 18,
+        xxl: 22,
+    }
+}
