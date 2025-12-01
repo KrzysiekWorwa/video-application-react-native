06643290ccf5d6004ec3cf74d8e3dcb3cf9a7505
Krzysztof Worwa <worwak1998@gmail.com>
2025-12-01 18:41:15 +0100

    chore: add Poppins font assets and configure font loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+
+  const [loaded, error] = useFonts({
+    Poppins_400Regular: require("../assets/fonts/Poppins-Regular.ttf"),
+    Poppins_500Medium: require("../assets/fonts/Poppins-Medium.ttf"),
+    Poppins_600SemiBold: require("../assets/fonts/Poppins-SemiBold.ttf"),
+    Poppins_700Bold: require("../assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }} />
+  );
 }
