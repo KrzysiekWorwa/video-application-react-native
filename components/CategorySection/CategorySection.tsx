0303d449295c8412bb9c5bfdeef3658026a45909
Krzysztof Worwa <worwak1998@gmail.com>
2025-12-03 00:38:58 +0100

    feat(category-section): implement CategorySection logic with YouTube API fetch and horizontal video list

--- components/CategorySection/CategorySection.tsx
+++ components/CategorySection/CategorySection.tsx
@@ -1,13 +1,74 @@
-import { Container, Header, ShowMoreText, TextWrapper } from "./CategorySection.styled";
+import { useFetch } from "@/hooks/useFetch";
+import { searchVideos, YoutubeVideo } from "@/services/api";
+import { useRouter } from "expo-router";
+import { FlatList, Image } from "react-native";
+import {
+    Card,
+    Container, Header,
+    PublishDate,
+    Separator,
+    ShowMoreText, TextWrapper, VideoTitle
+} from "./CategorySection.styled";
 
-export default function CategorySection() {
+type Props = {
+    title: string;
+    query: string;
+};
+
+export default function CategorySection({ title, query }: Props) {
+    const router = useRouter();
+
+    const { data, loading } = useFetch<YoutubeVideo[]>(
+        () => searchVideos(query, 10),
+        []
+    );
+
+    const handleShowMore = () => {
+        router.push({ pathname: "/search", params: { query: title } });
+    };
+
+    const renderItem = ({ item }: { item: YoutubeVideo }) => {
+        const date = new Date(item.publishedAt).toLocaleDateString("pl-PL");
+
+        return (
+            <Card activeOpacity={0.8}>
+                <Image
+                    source={{ uri: item.thumbnail }}
+                    style={{
+                        width: 220,
+                        height: 120,
+                        borderRadius: 12,
+                        backgroundColor: "#000",
+                    }}
+                />
+                <VideoTitle numberOfLines={2}>{item.title}</VideoTitle>
+                <PublishDate>{date}</PublishDate>
+            </Card>
+        );
+    };
 
     return (
         <Container>
             <TextWrapper>
-                <Header>React Native</Header>
-                <ShowMoreText>Show more</ShowMoreText>
+                <Header>{title}</Header>
+                <ShowMoreText onPress={handleShowMore}>Show more</ShowMoreText>
             </TextWrapper>
+
+            {loading && <Header>Loading...</Header>}
+
+            {!loading && data && (
+                <FlatList
+                    horizontal
+                    data={data}
+                    keyExtractor={(item) => item.id}
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={renderItem}
+                    style={{ marginTop: 12 }}
+                />
+            )}
+
+            <Separator />
+
         </Container>
     );
 }
