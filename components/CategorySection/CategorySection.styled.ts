e50b7badc69f5ff8b24bf2a456c1eddac58719e1
Krzysztof Worwa <worwak1998@gmail.com>
2025-12-03 00:25:28 +0100

    style(category-section): complete styles for video cards, headers and separator

--- components/CategorySection/CategorySection.styled.ts
+++ components/CategorySection/CategorySection.styled.ts
@@ -28,3 +28,31 @@ export const ShowMoreText = styled.Text`
     letter-spacing: 0.12px;
     text-decoration: underline;
 `;
+
+export const Card = styled.TouchableOpacity`
+    width: 180px;
+    margin-right: 16px;
+    margin-top: 24px;
+`;
+
+export const VideoTitle = styled.Text`
+    margin-top: 2px;
+    font-family: ${({ theme }) => theme.fonts.medium};
+    font-size: ${({ theme }) => theme.fontSizes.sm}px;
+    line-height: ${({ theme }) => theme.lineHeight.s}px;
+    letter-spacing: 0.12px;
+`;
+
+export const PublishDate = styled.Text`
+    margin-top: 4px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${({ theme }) => theme.fontSizes.xs}px;
+    line-height: ${({ theme }) => theme.lineHeight.l}px;
+    letter-spacing: 0.1px;
+`;
+
+export const Separator = styled.View`
+    margin-top: 24px;
+    height: 2px;
+    background-color: ${({ theme }) => theme.colors.primary};
+`;
