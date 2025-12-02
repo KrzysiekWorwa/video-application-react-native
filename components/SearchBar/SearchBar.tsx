b298c56cd22e74fc4f4c846ade1af13c2fdd5853
Krzysztof Worwa <worwak1998@gmail.com>
2025-12-02 17:57:45 +0100

    feat(search): enable navigation to Search screen from SearchBar

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,12 +1,18 @@
 import { Container, Input, SearchIcon } from "./SearchBar.styled";
 
-export default function SearchBar() {
+export default function SearchBar({ placeholder, onPress }: Props) {
+
+  interface Props {
+    placeholder: string;
+    onPress?: () => void;
+  }
+
   return (
     <Container>
       <SearchIcon width={24} height={24} />
       <Input
-        onPress={() => { }}
-        placeholder="Search videos"
+        onPress={onPress}
+        placeholder={placeholder}
         value=""
         onChangeText={() => { }} />
     </Container>
--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,12 +1,19 @@
+import { useRouter } from "expo-router";
 import { Text } from "react-native";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { Container, SearchBarWrapper } from "./Home.styled";
 
 export default function Home() {
+
+  const router = useRouter();
+
   return (
     <Container>
       <SearchBarWrapper>
-        <SearchBar />
+        <SearchBar 
+          onPress={() => router.push("/search")}
+          placeholder="Search videos"
+        />
       </SearchBarWrapper>
       <Text>Welcome to Home!</Text>
     </Container>
