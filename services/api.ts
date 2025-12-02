fb2021c00e2aa5cb05c56cf36995a77ce1de3310
Krzysztof Worwa <worwak1998@gmail.com>
2025-12-02 22:40:39 +0100

    feat(youtube): add YoutubeVideo type definition

--- services/api.ts
+++ services/api.ts
@@ -1,2 +1,11 @@
 const BASE_URL = "https://www.googleapis.com/youtube/v3";
 const API_KEY = process.env.EXPO_PUBLIC_YT_API_KEY;
+
+export type YoutubeVideo = {
+    id: string;
+    title: string;
+    thumbnail: string;
+    channelTitle: string;
+    publishedAt: string;
+    description?: string;
+};
