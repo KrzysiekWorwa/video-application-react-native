f9d36670de388c9de33be0e3444c8c49de7a3f21
Krzysztof Worwa <worwak1998@gmail.com>
2025-12-03 19:48:11 +0100

    feat: add basic VideoDetails screen with dynamic [id] route

---
+++ app/video/[id].tsx
@@ -0,0 +1,29 @@
+import { Stack, useLocalSearchParams } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function VideoDetailsScreen() {
+    const { id } = useLocalSearchParams<{ id: string }>();
+
+    return (
+        <>
+            <Stack.Screen options={{ title: "Video details" }} />
+
+            <View
+                style={{
+                    flex: 1,
+                    backgroundColor: "#050816",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <Text style={{ color: "#FFFFFF", fontSize: 18, fontWeight: "600" }}>
+                    Video details screen
+                </Text>
+
+                <Text style={{ color: "#9CA3AF", marginTop: 8 }}>
+                    id: {id ?? "brak id (jeszcze tu nie nawigujemy)"}
+                </Text>
+            </View>
+        </>
+    );
+}
