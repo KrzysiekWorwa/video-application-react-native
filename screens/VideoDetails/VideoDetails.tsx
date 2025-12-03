6225eb20de29012836655df47fc3119d25986877
Krzysztof Worwa <worwak1998@gmail.com>
2025-12-03 22:18:13 +0100

    feat(video): add react-native-video and initial video playback implementation

--- screens/VideoDetails/VideoDetails.tsx
+++ screens/VideoDetails/VideoDetails.tsx
@@ -1,9 +1,15 @@
 import { useFetch } from "@/hooks/useFetch";
 import { getVideoDetails } from "@/services/api";
 import { useLocalSearchParams } from "expo-router";
+import { Dimensions } from "react-native";
+import Video from "react-native-video";
 import { ChannelName, ChannelWrapper, Description, DescriptionTitle, IconCircle, InfoContaier, LikesIcon, PersonIcon, StatisticBox, StatisticText, StatisticWrapper, SwitchButton, SwitchLine, SwitchsWrapper, SwitchText, UnactiveSwitchLine, VideoTitle, ViewsIcon } from "./VideoDetails.styled";
 
+const { width } = Dimensions.get("window");
+const VIDEO_HEIGHT = width * 0.56;
+
 export default function VideoDetails() {
+
     const { id } = useLocalSearchParams<{ id: string }>();
 
     const { data: video, loading, error } = useFetch(
@@ -28,54 +34,68 @@ export default function VideoDetails() {
     }
 
     return (
-        <InfoContaier>
-            <VideoTitle numberOfLines={2}>
-                {video.title}
-            </VideoTitle>
+        <>
+            <Video
+                source={{ uri: "https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8" }}
+                style={{
+                    width: "100%",
+                    height: VIDEO_HEIGHT,
+                    backgroundColor: "#000",
+                }}
+                resizeMode="contain"
+                controls={true}
+                paused={false}
+                onError={(e) => console.log("VIDEO ERROR:", e)}
+            />
+            <InfoContaier>
+                <VideoTitle numberOfLines={2}>
+                    {video.title}
+                </VideoTitle>
 
-            <ChannelWrapper>
-                <IconCircle>
-                    <PersonIcon width={20} height={20} />
-                </IconCircle>
-                <ChannelName numberOfLines={1}>
-                    {video.channelTitle}
-                </ChannelName>
-            </ChannelWrapper>
+                <ChannelWrapper>
+                    <IconCircle>
+                        <PersonIcon width={20} height={20} />
+                    </IconCircle>
+                    <ChannelName numberOfLines={1}>
+                        {video.channelTitle}
+                    </ChannelName>
+                </ChannelWrapper>
 
-            <SwitchsWrapper>
-                <SwitchButton>
-                    <SwitchText>Details</SwitchText>
-                    <SwitchLine />
-                </SwitchButton>
+                <SwitchsWrapper>
+                    <SwitchButton>
+                        <SwitchText>Details</SwitchText>
+                        <SwitchLine />
+                    </SwitchButton>
 
-                <SwitchButton>
-                    <SwitchText>Notes</SwitchText>
-                    <UnactiveSwitchLine />
-                </SwitchButton>
-            </SwitchsWrapper>
+                    <SwitchButton>
+                        <SwitchText>Notes</SwitchText>
+                        <UnactiveSwitchLine />
+                    </SwitchButton>
+                </SwitchsWrapper>
 
-            <DescriptionTitle>Description</DescriptionTitle>
-            <Description>
-                {video.description || "No description available."}
-            </Description>
+                <DescriptionTitle>Description</DescriptionTitle>
+                <Description>
+                    {video.description || "No description available."}
+                </Description>
 
-            <DescriptionTitle>Statistics</DescriptionTitle>
+                <DescriptionTitle>Statistics</DescriptionTitle>
 
-            <StatisticWrapper>
-                <StatisticBox>
-                    <ViewsIcon width={20} height={20} />
-                    <StatisticText>
-                        {Number(video.stats.viewCount).toLocaleString()} views
-                    </StatisticText>
-                </StatisticBox>
+                <StatisticWrapper>
+                    <StatisticBox>
+                        <ViewsIcon width={20} height={20} />
+                        <StatisticText>
+                            {Number(video.stats.viewCount).toLocaleString()} views
+                        </StatisticText>
+                    </StatisticBox>
 
-                <StatisticBox>
-                    <LikesIcon width={20} height={20} />
-                    <StatisticText>
-                        {Number(video.stats.likeCount ?? 0).toLocaleString()} likes
-                    </StatisticText>
-                </StatisticBox>
-            </StatisticWrapper>
-        </InfoContaier>
+                    <StatisticBox>
+                        <LikesIcon width={20} height={20} />
+                        <StatisticText>
+                            {Number(video.stats.likeCount ?? 0).toLocaleString()} likes
+                        </StatisticText>
+                    </StatisticBox>
+                </StatisticWrapper>
+            </InfoContaier>
+        </>
     );
 }
