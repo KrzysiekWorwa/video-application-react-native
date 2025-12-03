72a467c91dcfd9d54f1e36b82c68bd1e37b6581a
Krzysztof Worwa <worwak1998@gmail.com>
2025-12-03 11:33:16 +0100

    feat: implement Search screen with working search logic and SearchSection list

--- screens/Search/Search.tsx
+++ screens/Search/Search.tsx
@@ -1,5 +1,9 @@
-import { useRouter } from "expo-router";
-import { Text } from "react-native";
+import { SearchSection } from "@/components/SearchSection/SearchSection";
+import { useFetch } from "@/hooks/useFetch";
+import { searchVideos, YoutubeVideo } from "@/services/api";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useState } from "react";
+import { ActivityIndicator, FlatList } from "react-native";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { Container, SearchBarWrapper } from "./Search.styled";
 
@@ -7,14 +11,43 @@ export default function Search() {
 
   const router = useRouter();
 
+  const params = useLocalSearchParams<{ query?: string }>();
+  const initialQuery = typeof params.query === "string" ? params.query : "";
+
+  const [searchText, setSearchText] = useState(initialQuery);
+
+  const { data, loading } = useFetch<YoutubeVideo[]>(
+    () => searchVideos(searchText || initialQuery, 20),
+    [searchText, initialQuery]
+  );
+
   return (
     <Container>
       <SearchBarWrapper>
-        <SearchBar 
+        <SearchBar
           placeholder="Search videos"
+          value={searchText}
+          onChangeText={setSearchText}
         />
       </SearchBarWrapper>
-      <Text>Welcome to Search page!</Text>
+
+      {loading && <ActivityIndicator style={{ marginTop: 24 }} />}
+
+      {!loading && data && (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <SearchSection
+              video={item}
+              onPress={() => {
+              }}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 24 }}
+        />
+      )}
     </Container>
   );
 }
